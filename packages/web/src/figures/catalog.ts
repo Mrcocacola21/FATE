17efd964335aeccc694aa3c2cf8389543cc68fe8
Soul <maxzenenkov@gmail.com>
2026-01-28 00:16:18 +0200

    Arts for stubs

--- packages/web/src/figures/catalog.ts
+++ packages/web/src/figures/catalog.ts
@@ -17,6 +17,31 @@ export const HERO_CATALOG: HeroCatalog = [
   { id: "elCidCompeador", name: "El Cid Compeador", mainClass: "knight" },
   { id: "lechy", name: "Lechy", mainClass: "trickster" },
   { id: "genghisKhan", name: "Genghis Khan", mainClass: "rider" },
+ 
+  { id: "duolingo", name: "Duolingo", mainClass: "trickster" },
+  { id: "luche", name: "Luche", mainClass: "spearman" },
+  { id: "donKigote", name: "Don Kihote", mainClass: "rider" },
+  { id: "jackRipper", name: "Jack Ripper", mainClass: "assassin" },
+  { id: "kaneki" , name: "Kaneki Ken", mainClass: "berserker" },
+  { id: "zoro" , name: "Roronoa Zoro", mainClass: "knight" },
+  { id: "artemida" , name: "Artemida", mainClass: "archer" },
+
+  { id: "mettaton", name: "Mettaton", mainClass: "archer" },
+  { id: "riverPerson", name: "River Person", mainClass: "rider" },
+  { id: "papyrus", name: "Papyrus", mainClass: "spearman" },
+  { id: "sans", name: "Sans", mainClass: "trickster" },
+  { id: "undyne", name: "Undyne", mainClass: "berserker" },
+  { id: "frisk", name: "Frisk", mainClass: "assassin" },
+  { id: "asgore", name: "Asgore", mainClass: "knight" },
+
+  { id: "odin" , name: "Odin", mainClass: "rider" },
+  { id: "loki" , name: "Loki", mainClass: "trickster" },
+  { id: "hassan" , name: "Hassan", mainClass: "assassin" },
+  { id:"guts" , name: "Guts", mainClass: "berserker" },
+  { id: "griffith" , name: "Griffith", mainClass: "knight" },
+  { id: "jebe" , name: "Jebe", mainClass: "archer" },
+  { id: "kaladin" , name: "Kaladin", mainClass: "spearman" },
+
   */
 ];
 
